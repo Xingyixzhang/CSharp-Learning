904e6a6bea4e7327a571cfb59e414627e53575ab
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2020-02-10 10:36:52 -0800

    Intro to delegates and anonymous delegates

---
+++ BasicDelegates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Delegates
+{
+    // Declare the delegate type:
+    public delegate string MyDelegate(int arg1, int arg2);
+
+    // Besides using static class functions, you can also use 
+    // instance methods of classes as delegates to implement delegates.
+    class MyClass
+    {
+        public string instanceMethod1(int arg1, int arg2)
+        {
+            return ((arg1 + arg2) * arg1).ToString();
+        }
+    }
+    class Program
+    {
+        // Insert delegates: 
+        static string func1(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        static string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+        static void Main(string[] args)
+        {
+            MyDelegate f = func1;
+            Console.WriteLine("The number is: " + f(10, 20));   // output is 30.
+
+            // Reassign the function f:
+            f = func2;
+            Console.WriteLine("The number is: " + f(10, 20));   // output is 200.
+
+            // To use the instanceMethod of class to implement delegates:
+
+            // First create an instance of the class MyClass, then assign 
+            // the delegate variable to the instance method.
+            MyClass mc = new MyClass();
+            f = mc.instanceMethod1;
+            Console.WriteLine("The number is: " + f(10, 20));   // output is 300.
+
+            //Anonymous Delegates:
+            MyDelegate AnonyF = delegate (int arg1, int arg2)
+            {
+                return (arg2 / arg1).ToString();
+            };
+            Console.WriteLine("The number is: " + AnonyF(10, 20));   // output: 2.
+
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
