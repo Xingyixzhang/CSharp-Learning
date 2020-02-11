d0e3631f3a286ed978627741c813658f3515bce8
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2020-02-11 09:21:42 -0800

    Event handler exercise
    1. Track the balance of a piggy bank;
    2. Watch changes on an object and respond;
    3. Inform when savings goal is met.
    
    output template:
    
    [Loop start]
    How much to deposite?
    <User Input>
    The balance amount is <total> / You've reached your savings goal! Now you have $<total>
    [Loop End]

---
+++ PiggyBank.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Event
+{
+    // defines the signature of the event handler.
+    public delegate void balanceEventHandler(int theValue);
+
+    class PiggyBank // broadcast changes in the balance.
+    {
+        public int myBalance;
+        public event balanceEventHandler balanceChanged;
+
+        public int Balance
+        {
+            set
+            {
+                myBalance = value;
+                balanceChanged(value);  // event triggered and change sent to all listeners.
+            }
+            get
+            {
+                return myBalance;
+            }
+        }
+    }
+
+    class BalanceLogger // logs the balance.
+    {
+        public void balanceLog(int total)   // int matches the signature of the event handler delegate.
+        {
+            Console.WriteLine("The balance amount is: {0}", total);
+        }
+    }
+
+    class BalanceWatcher    // if we have met the saving goal.
+    {
+        public void balanceWatch(int total) // int matches the signature of the event handler delegate.
+        {
+            if (total > 500) Console.WriteLine("You've met your savings goal! Now you have ${0}.", total);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PiggyBank pb = new PiggyBank();
+            BalanceLogger bl = new BalanceLogger();
+            BalanceWatcher bw = new BalanceWatcher();
+
+            pb.balanceChanged += bl.balanceLog;
+            pb.balanceChanged += bw.balanceWatch;
+
+            string str = "";
+            while (str != "exit")
+            {
+                Console.Write("How much to deposit?\t");
+                str = Console.ReadLine();
+                if (str != "exit")
+                {
+                    int value = int.Parse(str);
+                    pb.Balance += value;
+                } 
+            }
+
+            Console.WriteLine("\nPress Enter to continue...");
+        }
+    }
+}
