15166298c75926a256cf4dbc123feebf32ccbaf7
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2020-04-04 21:30:31 -0700

    Update Delegates.cs

--- Delegates and Events/Delegates.cs
+++ Delegates and Events/Delegates.cs
@@ -4,68 +4,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq;  // Language Integrated Query
 
-/*
-    Delegate is based on a MulticaseDelegate base class.
-    
-    A delegate is a specialized class functioning as a pipeline between event and event handler.
-    Event ======================= Delegate (Pipeline) ========================> Event Handler
-    
-    ** Multicast Delegate:
-    1. Can refernce >= 1 function
-    2. Tracks delegate references using an invocation list
-    3. Delegates in the list are invoked sequentially
-    
-    ** Delegate and Handler Method Parameters:
-    
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
-    public void Manager_WorkPerformed(int workHours, WorkType wType){ // Handler Method
-        ... ...
-    }
-    
-    ** Exposing and Raising Events:
-    
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
-    public class Workers{
-        public event WorkPerformedHandler WorkPerformed;    // Event Definition
-        public event EventHandler WorkCompleted;
-        
-        public virtual void DoWork(int hours, WorkType workType){
-            // Do work here and notify customers that work has been performed.
-            OnWorkPerformed(hours, workType);   // "On" is commonly used when Raising an event in .NET framework.
-            OnWorkCompleted():
-        }
-        
-        // "protected virtual" instead of "private": Allow method override for later.
-        protected virtual void OnWorkPerformed(int hours, WorkType workType){
-        
-            // Preferred Approach 
-            WorkPerformedHandler del = WorkPerformed as WorkPerformedHandler;
-            if (del != null){
-                del(hours, workType);       // Raise Event
-            }
-            
-            //Diff Approach:
-            if (WorkPerformed != null){
-                WorkPerformed(hours, workType);
-            }
-        }
-        
-        protected virtual void OnWorkCompleted(object sender, EventArgs e){
-        
-            // Preferred Approach 
-            EventHandler del = WorkCompleted as EventHandler;
-            if (del != null){
-                del(this, EventArgs.Empty);       // Raise Event
-            }
-            
-            //Diff Approach:
-            if (WorkCompleted != null){
-                WorkCompleted(this, EventArgs.Empty);
-            }
-        }
-    }
-*/
-
 namespace Delegates
 {
     // One way pipeline without any return type. Indicates that the methods (event handlers are void as well)
