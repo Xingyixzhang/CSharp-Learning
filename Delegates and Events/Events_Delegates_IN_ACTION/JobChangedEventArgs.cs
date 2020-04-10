f28796bae0991afc13dba5c19046509f9c84d305
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2020-04-10 14:58:27 -0700

    Update JobChangedEventArgs.cs

--- Delegates and Events/Events_Delegates_IN_ACTION/JobChangedEventArgs.cs
+++ Delegates and Events/Events_Delegates_IN_ACTION/JobChangedEventArgs.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text;
+using CommunicatingBetweenControls.Model;
 
-namespace CommunicatingBetweenControls.Model{
+namespace CommunicatingBetweenControls
+{
     public class JobChangedEventArgs: EventArgs
     {
         public Job Job { get; set; }
